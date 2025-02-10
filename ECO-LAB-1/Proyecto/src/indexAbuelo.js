dda492eb0c5b7cbc3703b0b83fa17cf3b41123ef
Sergio Restrepo Prado <153745435+SergioRP18@users.noreply.github.com>
2025-02-10 10:03:21 -0500

    Add files via upload
    avances

---
+++ ECO-LAB-1/Proyecto/src/indexAbuelo.js
@@ -0,0 +1,41 @@
+import { getDataFetch1, getDataFetch2, getDataFetch3 } from "./services/fetch";
+
+class AppContainer extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    async render(){
+        if(this.shadowRoot){
+
+            const title = document.createElement("h1");
+            title.innerText = "Mis Llamadas de API's";
+            this.shadowRoot.appendChild(title);
+
+            const section = document.createElement("section");
+            this.shadowRoot.appendChild(section);
+
+            const buttonApi1 = document.createElement("button");
+            buttonApi1.innerText = "FETCH API 1";
+            buttonApi1.addEventListener("click", getDataFetch1.bind(this));
+            section.appendChild(buttonApi1);
+
+            const buttonApi2 = document.createElement("button");
+            buttonApi2.innerText = "FETCH API 2";
+            buttonApi2.addEventListener("click", getDataFetch2.bind(this));
+            section.appendChild(buttonApi2);
+
+            const buttonApi3 = document.createElement("button");
+            buttonApi3.innerText = "FETCH API 3";
+            buttonApi3.addEventListener("click", getDataFetch3.bind(this));
+            section.appendChild(buttonApi3);
+        }
+    }
+};
+
+customElements.define("app-container", AppContainer);
---
+++ ECO-LAB-1/Proyecto/src/services/fetch.js
@@ -0,0 +1,70 @@
+export async function getDataFetch1() {
+    renderLoadingState();
+
+    try {
+        const response = await fetch ("https://randomuser.me/api/");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        renderDataFetch(data);
+    } catch(error) {
+        renderErrorState();
+    }
+};
+
+export async function getDataFetch2() {
+    renderLoadingState();
+
+    try {
+        const response = await fetch ("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        renderDataFetch(data);
+    } catch(error) {
+        renderErrorState();
+    }
+};
+
+export async function getDataFetch3() {
+    renderLoadingState();
+
+    try {
+        const response = await fetch ("https://api.jikan.moe/v4/anime");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        renderDataFetch(data);
+    } catch(error) {
+        renderErrorState();
+    }
+};
+
+const renderErrorState = () => {
+    const error = document.querySelector(".app-container");
+    error.innerHTML = "";
+    error.innerText = "Not Found";
+    console.log("Not Found");
+};
+
+
+const renderLoadingState = () => {
+    const loading = document.querySelector(".app-container");
+    loading.innerHTML = "";
+    loading.innerText = "Loading...";
+    console.log("Loading...");
+};
+
+
+const renderDataFetch = () => {
+    const dataInfo = document.querySelector(".app-container");
+    dataInfo.innerHTML = "";
+
+}
+
+getDataFetch1();
+getDataFetch2();
+getDataFetch3();
